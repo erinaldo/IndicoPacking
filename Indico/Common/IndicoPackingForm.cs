0ec8cd942a4b6d693dc4c3fc0afdde843b6ed736
rusith <rusith@mail.com>
2016-10-09 08:19:38 +0530

    changed filling carton polybag validate

---
+++ Indico/Common/IndicoPackingForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IndicoPacking.Common
+{
+    public class IndicoPackingForm:Form
+    {
+        protected SqlConnection IndicoPackingConnection
+        {
+            get
+            {
+                var connection =new SqlConnection(ConfigurationManager.ConnectionStrings["IndicoConnString"].ConnectionString);
+                return connection;
+            }
+        }
+    }
+}
